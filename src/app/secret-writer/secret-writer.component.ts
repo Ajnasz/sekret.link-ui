6b24f561e1c8bee5b4c2d105cab1559519cd4b09
Lajos Koszti <ajnasz@ajnasz.hu>
2020-08-30 11:17:41 +0200

    Add some error handling

--- src/app/secret-writer/secret-writer.component.ts
+++ src/app/secret-writer/secret-writer.component.ts
@@ -1,7 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
 
 import { Secret } from '../secret';
 import { SecretService } from '../secret.service';
+
+const ONE_HOUR = '1h';
+const ONE_DAY = '24h';
+const ONE_WEEK = '168h';
+const THIRTY_DAYS = '720h';
+
+type EXPIRE_DURATIONS = '1h' | '24h' | '168h' | '720h';
+type ExpireDurationValue = {
+  text: string;
+  value: EXPIRE_DURATIONS;
+};
 
 @Component({
   selector: 'app-secret-writer',
@@ -9,16 +21,17 @@ import { SecretService } from '../secret.service';
   styleUrls: ['./secret-writer.component.css']
 })
 export class SecretWriterComponent implements OnInit {
+    errorMessage = '';
     secret: Secret = null;
     newURL = '';
     copied = false;
-    expirations = [
-      {text: '1 Hour', value: '1h'},
-      {text: '1 Day', value: '24h'},
-      {text: '1 Week', value: `${24 * 7}h`},
-      {text: '30 Days', value: `${24 * 7 * 30}h`},
+    expirations: ExpireDurationValue[] = [
+      {text: '1 Hour', value: ONE_HOUR},
+      {text: '1 Day', value: ONE_DAY},
+      {text: '1 Week', value: ONE_WEEK},
+      {text: '30 Days', value: THIRTY_DAYS},
     ];
-    selectedExpiration = '1d';
+    selectedExpiration: EXPIRE_DURATIONS = '24h';
 
     get isSecretReady(): boolean {
       return this.newURL !== '';
@@ -29,9 +42,26 @@ export class SecretWriterComponent implements OnInit {
     ) {
     }
 
+    validateSecret(): boolean {
+      if (this.secret.Data === '') {
+        this.errorMessage = 'Secret must not be empty';
+        return false;
+      }
+
+      this.errorMessage = '';
+
+      return true;
+    }
+
     onSubmit(): void {
+        if (!this.validateSecret()) {
+          return;
+        }
+
         this.secretService.saveSecret(this.secret.Data, this.selectedExpiration).subscribe((secret: Secret) => {
           this.newURL = `${window.location.protocol}//${window.location.host}/view/${secret.UUID}#${secret.Key}`;
+        }, (error) => {
+          this.errorMessage = error.error;
         });
     }
 
@@ -49,7 +79,7 @@ export class SecretWriterComponent implements OnInit {
       this.copied = false;
     }
 
-    updateSelectedExpirateion(value: string): void {
+    updateSelectedExpiration(value: EXPIRE_DURATIONS): void {
       this.selectedExpiration = value;
     }
 
