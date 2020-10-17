34ffeb080a3a4cbb46eac8e7cfb3fa11160bb7bd
Lajos Koszti <ajnasz@ajnasz.hu>
2020-10-17 20:47:44 +0200

    Load privacy policy on demand

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,16 @@ import { SecretWriterComponent } from './secret-writer/secret-writer.component';
 import { SecretViewerComponent } from './secret-viewer/secret-viewer.component';
 import { SecretCreatedComponent } from './secret-created/secret-created.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { PrivacyComponent } from './privacy/privacy.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/create', pathMatch: 'full' },
     { path: 'create', component: SecretWriterComponent },
     { path: 'created', component: SecretCreatedComponent },
     { path: 'view/:id', component: SecretViewerComponent },
-    { path: 'privacy', component: PrivacyComponent },
     { path: '404', component: NotFoundComponent },
     { path: 'hidden', redirectTo: '/create', pathMatch: 'full' },
     { path: 'api-doc', loadChildren: () => import('./api-documentation/api-documentation.module').then(m => m.ApiDocumentationModule) },
+    { path: 'privacy', loadChildren: () => import('./privacy/privacy.module').then(m => m.PrivacyModule) },
     { path: '**', redirectTo: '/404' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { SecretViewerComponent } from './secret-viewer/secret-viewer.component';
 import { SecretComponent } from './secret/secret.component';
 import { SecretWriterComponent } from './secret-writer/secret-writer.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { PrivacyComponent } from './privacy/privacy.component';
 import { PageFooterComponent } from './page-footer/page-footer.component';
 import { IcomoonIconComponent } from './icomoon-icon/icomoon-icon.component';
 import { PageLogoComponent } from './page-logo/page-logo.component';
@@ -23,7 +22,6 @@ import { PageErrorComponent } from './page-error/page-error.component';
     SecretComponent,
     SecretWriterComponent,
     NotFoundComponent,
-    PrivacyComponent,
     PageFooterComponent,
     IcomoonIconComponent,
     PageLogoComponent,
